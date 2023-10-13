01489389e82d1a73023c3f4e34ff1b5c41af2236
AntonioMangado <antonio.mangado@gmail.com>
2023-10-13 14:06:54 +0400

    FINISH/ exercises

--- script.js
+++ script.js
@@ -20,47 +20,28 @@ form.addEventListener("submit", function(event) {
     let user = JSON.parse(localStorage.getItem("user"));
     document.getElementById("datos-container").innerHTML = `${user.Nombre}, ${user.Email}, ${user.Observaciones}`
 })
-// let userDatos= Object.values(event.target.elements.value);
-    
-// for (let i = 0; i < userDatos.length; i++) {
-//     console.log(userDatos[i])
-// }
-// 
 
 
-/*let perro = {
-    nombre: "Scott",
-    color: "Negro",
-    macho: true,
-    edad: 5
-  };
-  
-  let valores = Object.values(perro); // valores = ["Scott", "Negro", true, 5];
-  for(let i=0; i< valores.length; i++){
-    console.log(valores[i]);
-  }
-  /*
-  "Scott"
-  "Negro"
-  true
-  5
-  */
+//BORRAR TODOS LOS DATOS DEL LOCAL STORAGE
+document.getElementById("borrarAll").addEventListener("click", function(){
+  localStorage.clear();
+  console.log("Datos borrados");
+});
 
-/*
-  let perro = {
-    nombre: "Scott",
-    color: "Negro",
-    macho: true,
-    edad: 5
-  };
+//BORRAR UN UNICO USUARIO
+document.querySelector(".borrarUser-form").addEventListener("submit", function (event) {
+  // event.preventDefault();
+  // console.log(event);
+  let userToDelete = event.target[0].value
+  let userData = JSON.parse(localStorage.getItem("user"))
   
-  for(const [key, value] of Object.entries(perro)){
-    console.log(value)
-  }
+
+    if (userToDelete == userData.Nombre) {
+      localStorage.removeItem("user")
+    }
   
-  /*salida:
-  "Scott"
-  "Negro"
-  true
-  5
-  */
+})
+
+
+
+
