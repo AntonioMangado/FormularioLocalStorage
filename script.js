039f16ccb9ffde3f980400446fe918bc552ffe1a
AntonioMangado <antonio.mangado@gmail.com>
2023-10-18 16:56:53 +0400

    add/formatting

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ let form = document.getElementById("form");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log(event.target[0].value);
-    // target.elements[0].value // Value del nombre
-    // target.elements[1].value // Value del mail
-    // target.elements[2].value // Value del comment
-    
-    // localStorage.setItem("Nombre", `${event.target[0].value}`)
-    // localStorage.setItem("Email", `${event.target[1].value}`)
-    // localStorage.setItem("Observaciones", `${event.target[2].value}`)
-
     localStorage.setItem("user", JSON.stringify({
         Nombre: `${event.target[0].value}`,
         Email:`${event.target[1].value}`,
@@ -18,9 +10,8 @@ form.addEventListener("submit", function(event) {
     }))
         
     let user = JSON.parse(localStorage.getItem("user"));
-    document.getElementById("datos-container").innerHTML = `${user.Nombre}, ${user.Email}, ${user.Observaciones}`
+    document.getElementById("datos-container").innerHTML += `${user.Nombre}, ${user.Email}, ${user.Observaciones}`
 })
-
 
 //BORRAR TODOS LOS DATOS DEL LOCAL STORAGE
 document.getElementById("borrarAll").addEventListener("click", function(){
